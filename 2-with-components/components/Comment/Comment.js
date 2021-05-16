a2ec91f483bf6f621570df6797a63853c2b3c1f0
Andrey Vorobyev <allah.taskbar@gmail.com>
2021-05-16 20:31:51 +0300

    Fix date view

--- 2-with-components/components/Comment/Comment.js
+++ 2-with-components/components/Comment/Comment.js
@@ -12,7 +12,7 @@ class Comment extends Component {
                 <div class='comment__author-and-date'>
                     <span class='comment__author'>${this.props.author}</span>
                     <span class='comment__date'>
-                        (${date.getDay()}.${date.getMonth()}.${date.getFullYear()})
+                        (${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()})
                     </span>
                 </div>
                 <div class='comment__text'>${this.props.text}</div>
